c9ee3fb13c87f6067c9f6302ba00774d4152a7d0
Soskufu <111096249+Soskufu@users.noreply.github.com>
2024-07-24 04:40:52 -0400

    better controled snake
    wierd pause when collecting apple now
    :(

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,38 @@
 #include <deque>
 
 bool running = true;
-
+float SNAKE_BASE_SPEED = 0.2f;
+float SNAKE_MAX_SPEED = 0.05f;
+float SPEED_BUMP = 0.005f;
 SDL_Renderer* renderer;
 SDL_Window* window;
-SDL_Rect head {500,500,30,30},apple{0,0,30,30};
+int width = 900;
+int height = 900;
+int CELL = 30;
+SDL_Rect head {10 * CELL,10 * CELL,CELL,CELL},apple{0,0,CELL,CELL};
 std::deque<SDL_Rect> Snakebody; //snake body container
-unsigned int size = 0;
+unsigned int size = 1;
+int score = 0;
+float moveCounter = SNAKE_BASE_SPEED;
 enum Directions {up = 1,down,left,right};
+
+struct Timestep
+{
+public:
+	Timestep(float time = 0.0f)
+	: m_Time(time)
+	{
+
+	}
+	float GetSeconds() const { return m_Time/1000;}
+	float GetMilliseconds() const{return m_Time;}
+private:
+	float m_Time;
+};
 
 void createWindow()
 {
-	if (SDL_CreateWindowAndRenderer(1000, 1000, 0, &window, &renderer) < 0)
+	if (SDL_CreateWindowAndRenderer(width, height, 0, &window, &renderer) < 0)
 	{ 
 		std::cout << "Failed at SDL_CreateWindowAndRenderer()" << std::endl;
 	}
@@ -37,18 +58,28 @@ SDL_SetRenderDrawColor(renderer,0,0,0,255);
 SDL_RenderClear(renderer);
 }
 void gen_apple()
-{   
-	int randomX = rand() % 900;
-    int randomY = rand() % 900;
+{   while(SDL_HasIntersection(&apple, &head))
+{
+	int randomX = rand() % (width/ CELL) * CELL;
+	//randomX *= rand() % 30;
+    int randomY = rand() % (height/ CELL) * CELL; 
+    //randomY *= rand() % 30;
     apple.x = randomX;
     apple.y = randomY;
+    size++;
 }
-void startover(int &move) {
-   head.x = 500;
-   head.y = 500;
+}
+
+
+
+void startover(int &move) 
+{
+   head.x = width/2;
+   head.y = height/2;
    move = 0;
-   size = 0;
-   gen_apple();
+   score = 0;
+   size = 1;
+   SNAKE_BASE_SPEED = 0.2f;
  }
 
 void input(int &move)
@@ -71,21 +102,16 @@ void logic(int &move)
 {
 	switch(move)
 		{
-			case down: head.y += 8; break;
-			case up: head.y -= 8; break;
-			case left: head.x -= 8; break;
-			case right: head.x += 8; break;
+			case down: head.y += CELL; break;
+			case up: head.y -= CELL; break;
+			case left: head.x -= CELL; break;
+			case right: head.x += CELL; break;
 		};
-}
-void update(int &move) {    
-    if (SDL_HasIntersection(&apple, &head)) {
+		/* if (SDL_HasIntersection(&apple, &head)) {
+        //speed += speedbump;
         gen_apple();
-        if(size == 0)
-        {
-        size += 2;
-        } 
-        else{}size++;
-    }
+        score++;
+    }*/
     //collision detection with snake body
     for(auto snake_segment : Snakebody){ 
 			if(head.x == snake_segment.x && head.y == snake_segment.y)
@@ -96,26 +122,48 @@ void update(int &move) {
     //add newest head to the snake
 	Snakebody.push_front(head);
 	while(Snakebody.size() > size)
+	{
 	Snakebody.pop_back();
-    
+    }
     if (head.x < 0)  startover(move);////head.x +=1000;
-    if (head.x > 1000)  startover(move);//head.x-=1000;
+    if (head.x > width)  startover(move);//head.x-=1000;
     if (head.y < 0)  startover(move); //head.y +=1000;
-    if (head.y > 1000)  startover(move); //head.y-=1000;
+    if (head.y > height)  startover(move); //head.y-=1000;
 }
+void update(int & move,Timestep ts) {
+    moveCounter -= ts.GetSeconds();
 
+    if (moveCounter <= 0) {
+        logic(move);
+        moveCounter = SNAKE_BASE_SPEED - (size * SPEED_BUMP);
+        moveCounter = std::max(moveCounter, SNAKE_MAX_SPEED); //takes the bigger number of the two
+        std::cout << moveCounter << std::endl;
+    }
+}
+void vector2f(SDL_Rect bob)
+{
+	int x = bob.x/30.0;
+	int y = bob.y/30.0;
+	std::cout << "(" << x << "," << y << ")\n";
+}
 int WinMain(int argc,char* args[])
 {
+	
+	float m_LastFrameTime = SDL_GetTicks();
 	int move = 0;
 	createWindow();
 	gen_apple();
 	while(running)
 	{
+		float time = (float)SDL_GetTicks(); //current frametime
+		Timestep timestep = (time - m_LastFrameTime);//deltatime
+		m_LastFrameTime = time;//record last frametime
+		std::cout << timestep.GetSeconds() << std::endl;
 		input(move);
-		logic(move);
-		update(move);
+		update(move,timestep);
+		gen_apple();
 		render();//draw Body
-		SDL_Delay(25);
+		SDL_Delay(17);
 	}
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
